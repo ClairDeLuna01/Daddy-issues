313abce25f616e18a217d4a7662358ca1168d5ba
mwet <luca.chaudillon@etudiant.univ-perp.fr>
2023-12-02 09:47:20 +0100

    Changed energy model + Gummy bears now look at you

--- Assets/Scripts/ArmorConsumable.cs
+++ Assets/Scripts/ArmorConsumable.cs
@@ -1,8 +1,19 @@
 using UnityEngine;
+using System;
 
 public class ArmorConsumable : Consumable
 {
+    private float frame = 0f;
+	private Vector3 startPos;
+	private Player playerToLook;
+
     // Start is called before the first frame update
+
+	protected override void Start() {
+		base.Start();
+		startPos = transform.position;
+		playerToLook = GameObject.Find("Player").GetComponent<Player>();
+	}
 
 	void OnTriggerEnter(Collider other) {
 
@@ -14,6 +25,21 @@ public class ArmorConsumable : Consumable
 			Destroy(gameObject);
 		}
 
+	}
+
+	void Animate() {
+		float offset = (float)Math.PI/180f * frame;
+		transform.position = startPos + new Vector3 (0f, (float)Math.Sin(offset)/4f + 0.5f, 0f);
+		Vector3 rot = transform.rotation.eulerAngles;
+        float newRot = Quaternion.LookRotation(playerToLook.transform.position - transform.position).eulerAngles.y;
+        // interpolate the rotation so it's not instant
+        rot.y = Mathf.LerpAngle(rot.y, newRot, 10f * Time.deltaTime);
+        transform.rotation = Quaternion.Euler(rot);
+		frame = (frame + Time.deltaTime*100f) % 360;
+	}
+
+	void Update() {
+		Animate();
 	}
 
 }
--- Assets/Scripts/EnergyConsumable.cs
+++ Assets/Scripts/EnergyConsumable.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
+using System;
 
 public class EnergyConsumable : Consumable
 {
 	public int amount = 25;
+	private float frame = 0f;
+	private Vector3 startPos;
 
     // Start is called before the first frame update
 
+	protected override void Start() {
+		base.Start();
+		startPos = transform.position;
+	}
 	void OnTriggerEnter(Collider other) {
 
 		Player player = other.transform.GetComponent<Player>();
@@ -16,6 +23,17 @@ public class EnergyConsumable : Consumable
 			Destroy(gameObject);
 		}
 
+	}
+
+	void Animate() {
+		float offset = (float)Math.PI/180f * frame;
+		transform.position = startPos + new Vector3 (0f, (float)Math.Sin(offset)/4f + 0.5f, 0f);
+		transform.RotateAround(transform.position, new Vector3 (0f, 1f, 0f), Time.deltaTime*100f);
+		frame = (frame + Time.deltaTime*100f) % 360;
+	}
+
+	void Update() {
+		Animate();
 	}
 
 }
