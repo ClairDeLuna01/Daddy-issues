74712fd67c8084561565c0a6a15f20ed5ff27920
mwet <luca.chaudillon@etudiant.univ-perp.fr>
2023-12-02 09:48:15 +0100

    forgot to save changes

--- Assets/Scripts/Consumable.cs
+++ Assets/Scripts/Consumable.cs
@@ -3,11 +3,8 @@ using UnityEngine;
 public class Consumable : MonoBehaviour 
 {
 	private Collider objectCollider;
-	protected GameManager gameManager;
 	
-
 	protected virtual void Start() {
-		gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
 		objectCollider = GetComponent<Collider>();
 		if(objectCollider != null) {
 			objectCollider.isTrigger = true;
